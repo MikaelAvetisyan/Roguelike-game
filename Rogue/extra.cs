e5074761a85329a0efbffe2f3b31348ba3d4e378
Mikael Avetisyan <Mikael.avetisyan@elev.ga.ntig.se>
2025-10-30 02:50:55 +0100

    Added defense + market update

--- Rogue/extra.cs
+++ Rogue/extra.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Intrinsics.X86;
 using System.Security;
 using System.Threading;
 
@@ -35,12 +36,40 @@ class Character
     }
 }
 
+class Black
+{
+    public static void Market()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("Market");
+        Console.ResetColor();
+        Console.ReadLine();
+
+        Console.WriteLine("Welcome to the ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("Market");
+        Console.ResetColor();
+        Console.WriteLine("Here you can buy Goods ");
+        Console.WriteLine("like items that can aid you in battle");
+        Console.WriteLine("");
+        Console.WriteLine("Or upgrades like");
+        Console.WriteLine("Weapon upgrades");
+        Console.WriteLine("Health upgrades");
+        Console.WriteLine("And so on");
+
+        // continue here
+    }
+}
+
 class Action
 {
+    public static int round = 1;
+    public static int Enemy_1 = 100;
+    public static int Enemy_2 = 100;
+    public static int Enemy_damage = 10;
     public static void Text(Character player)
     {
-        int Enemy_1 = 100;
-        int Enemy_2 = 100;
+
         while (true)
         {
             Console.WriteLine("---- Choose Your action ----");
@@ -74,11 +103,14 @@ class Action
             if (answer == "attack")
             {
                 int damage = player.Attack;
+
                 while (true)
                 {
                     Console.WriteLine("Which skeleton do you attack");
                     Console.WriteLine("Skeleton 1 or Skeleton 2");
+
                     string attack_answer = Console.ReadLine().Trim().ToLower();
+
                     if (attack_answer == "skeleton 1")
                     {
                         Enemy_1 -= damage;
@@ -115,21 +147,27 @@ class Action
                 }
                 break;
             }
+
             else if (answer == "defend")
             {
-                
+                Console.WriteLine("You activated your Defense stance");
+                Console.WriteLine("Defense stance = Your oponents deal half damage for 2 round");
+                bool defense_stance = true;
                 break;
             }
+
             else if (answer == "item")
             {
                 // Add item logic here
                 break;
             }
+
             else if (answer == "special")
             {
                 // Add special logic here
                 break;
             }
+
             else
             {
                 Console.Clear();
@@ -138,5 +176,22 @@ class Action
             }
             Console.ReadLine();
         }
+
+        Console.WriteLine("!! The skeletons strikes back !!");
+        Console.WriteLine($"The skeletons did -{Enemy_damage}");
+        player.Health -= Enemy_damage;
+        Console.WriteLine($"Your HP = {player.Health}");
+        Console.ReadLine();
+
+        if (Enemy_1 >= 0 && Enemy_2 >= 0)
+        {
+            Console.WriteLine("!!! Round cleared !!!");
+            Console.WriteLine("Moving on to the next room");
+            Console.ReadLine();
+            Console.WriteLine("To the ");
+            round++;
+
+        }
     }
 }
+
